d7f5996503a0b0e76e8b724fca9e69b2158a0a46
github username <yinagit@gmail.com>
2019-12-23 23:13:37 -0500

    tested model works

--- expenses-api/db/seeds.rb
+++ expenses-api/db/seeds.rb
@@ -3,3 +3,5 @@ account = Account.create(name: 'Checking', balance: 1000)
 transaction = Transaction.create(account_id: 1, amount: 10, kind: 'deposit', date: Date.today, description: 'Found $10')
 
 transaction_two = Transaction.create(account_id: 1, amount: 10, kind: 'withdraw', date: Date.today, description: 'Found $10')
+
+transaction_two = Transaction.create(account_id: 1, amount: 10, kind: 'random', date: Date.today, description: 'Boop $10')
