a49db65f6851696df1403dfc20f6a0b32b7fec71
github username <yinagit@gmail.com>
2019-12-24 23:38:10 -0500

    setting up Transaction controller

--- expenses-api/app/controllers/api/v1/transactions_controller.rb
+++ expenses-api/app/controllers/api/v1/transactions_controller.rb
@@ -1,22 +1,34 @@
 class Api::V1::TransactionsController < ApplicationController
 
   def index
+    @transactions = Transaction.all
+    render json: @transactions
   end
 
   def create
-
+    @transaction = Transaction.new(transaction_params)
+    if @transaction.save
+      render json: @transaction
+    else
+      render json: (error: "Error creating transaction")
+    end
   end
 
   def show
+    @transacton = Transaction.find(params[:id])
+    render json: @transaction
   end
 
   def destroy
+    @transaction = Transaction.find(params[:id])
+    @transaction.destroy
   end
 
 
   private
 
   def transaction_params
+    params.require(:transation).permit(:account_id, :amount, :kind, :date, :description)
   end
 
 
