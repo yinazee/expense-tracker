141866c7a72b6c1573becce1a81a0ecc00a028fa
github username <yinagit@gmail.com>
2019-12-24 23:12:31 -0500

    added update_balance method in account model

--- expenses-api/app/models/account.rb
+++ expenses-api/app/models/account.rb
@@ -2,4 +2,18 @@ class Account < ApplicationRecord
   has_many :transations
   validates :name, :balance, presence: true
 
+  def update_balance(transaction)
+    if transaction.kind == 'deposit'
+      self.balance = self.balance + transaction.amount
+      self.save
+    elsif transaction.kind == 'withdraw'
+      if self.balance >= transaction.amount
+        self.balance = self.balance - transaction.amount
+        self.save
+      else
+        return 'balance too low'
+      end
+    end
+  end
+
 end
