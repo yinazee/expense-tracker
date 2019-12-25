99dc47ebce23615cd5a0dbc322503135ea9e42aa
github username <yinagit@gmail.com>
2019-12-24 23:24:46 -0500

    set up Account controller

--- expenses-api/app/controllers/api/v1/transactions_controller.rb
+++ expenses-api/app/controllers/api/v1/transactions_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::TransactionsController < ApplicationController
+
+  def index
+  end
+
+  def create
+
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def transaction_params
+  end
+
+
+
 end
