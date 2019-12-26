c312bb1c6d29b9d8c6fe7045517c87d724f12966
github username <yinagit@gmail.com>
2019-12-25 21:59:21 -0500

    fixed App.js syntax issue

--- expenses-api/app/controllers/api/v1/transactions_controller.rb
+++ expenses-api/app/controllers/api/v1/transactions_controller.rb
@@ -8,6 +8,13 @@ class Api::V1::TransactionsController < ApplicationController
 
   def create
     @transaction = @account.transaction.new(transaction_params)
+    if @account.update_balance(transaction) != "Balance too low."
+      @transaction.save
+      render json: @transaction
+    else
+      render json: {error: 'Balance too low.'}
+    end
+
   end
 
   def show
