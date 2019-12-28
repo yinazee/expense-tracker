d7c2a22988d8539c867ce237baa7e21a5cc20c72
github username <yinagit@gmail.com>
2019-12-28 01:39:00 -0500

    fixed syntax issue in accounts controller

--- expenses-api/app/controllers/api/v1/accounts_controller.rb
+++ expenses-api/app/controllers/api/v1/accounts_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::AccountsController < ApplicationController
     if @account.save
       render json: @accounts
     else
-      render json: (error: "Error creating account")
+      render json: {error: "Error creating account"}
     end
   end
 
--- expenses-client/src/App.js
+++ expenses-client/src/App.js
@@ -2,6 +2,19 @@ import React from 'react';
 
 
 class App extends React.Component {
+
+  // )
+// .then(resp => resp.json())
+// .then(data => console.log(data))
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/accounts/1', {
+    method: 'GET'
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+  }
   render() {
       return (
         <div className="App">
