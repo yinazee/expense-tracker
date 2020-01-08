d8e256b93f43366f02d87d79d5999fb50be80549
github username <yinagit@gmail.com>
2020-01-07 21:24:09 -0500

    added setState and onChange handlers to AccountInput form

--- expenses-client/src/components/AccountInput.js
+++ expenses-client/src/components/AccountInput.js
@@ -2,15 +2,26 @@ import React from 'react'
 
 class AccountInput extends React.Component {
 
+  state = {
+    name: '',
+    balance: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <form>
           <label>Checking Account Name:</label>
-          <input type="text" placeholder="Name"/>
+          <input type="text" placeholder="Name" name="name" value={this.state.name} onChange={this.handleChange}/>
           <br/>
           <label>Checking Account Balance:</label>
-          <input type="text" placeholder="Balance"/>
+          <input type="text" placeholder="Balance" name="balance" onChange={this.handleChange}/>
         </form>
       </div>
     )
