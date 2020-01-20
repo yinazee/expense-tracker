a01ab06831354bf89a7b917ad5b929da7f2eac28
github username <yinagit@gmail.com>
2020-01-19 23:36:14 -0500

    Starting to add Routes

--- expenses-client/src/index.js
+++ expenses-client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
 import accountReducer from './reducers/accountReducer'
 
 
@@ -15,7 +16,9 @@ let myStore = createStore(accountReducer, composeEnhancers(applyMiddleware(thunk
 
 ReactDOM.render(
   <Provider store={myStore}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>
   ,
   document.getElementById('root'))
